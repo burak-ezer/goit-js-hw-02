6f05fd6ec21103a8684bb75dde28b7ddba07ed3d
burak-ezer <burakezerrr@gmail.com>
2025-05-06 00:50:53 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function checkForSpam(message) {
     if(message.toLowerCase().search("spam")!=-1||message.toLowerCase().search("sale")!=-1){
+        console.log(true);
         return true;
         
     }else {
+        console.log(false);
         return false;
     }
 }
